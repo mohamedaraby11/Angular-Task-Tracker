38025ca23cc3ef58540e960217b8ae4a29eceb4b
mohamed <mohamed.araby288@gmail.com>
2022-11-12 01:01:23 +0200

    adding routern

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,19 +9,24 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title : string = 'Task Tracker';
-  showAddTask : boolean = false;
-  subscription : Subscription;
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor(private uiService:UiService) {
-    this.subscription = this .uiService.onToggle().subscribe((value)=>(this.showAddTask = value))
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value))
   }
 
   ngOnInit(): void {
   }
 
-  toggleAddTask(){
-this.uiService.toggleAddTask();
- }
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route
+
+  }
 
 }
