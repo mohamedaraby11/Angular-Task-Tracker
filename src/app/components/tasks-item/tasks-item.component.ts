b62f137894828048067c496a21cf059f76dda4b9
mohamed <mohamed.araby288@gmail.com>
2022-11-11 22:13:28 +0200

    Add And Delete Functions

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit,Input ,Output,EventEmitter} from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+@Input() task:Task
+@Output() onDeleteTask : EventEmitter<Task> = new EventEmitter
+
+@Output() onToggleReminder : EventEmitter<Task> = new EventEmitter
+faTimes = faTimes;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task){
+    this.onDeleteTask.emit(task);
+  }
+  onToggle(task){
+    this.onToggleReminder.emit(task);
+  }
+
+}
